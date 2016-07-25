9746bac059895cc4425c6c5b6fef46b3567dd3b9
pacebook <haegyun821@gmail.com>
2016-07-25 23:23:39 +0900

    마지막 테스트

--- pyscripts/save_timetable.py
+++ pyscripts/save_timetable.py
@@ -15,14 +15,14 @@ if 'DJANGO_SETTINGS_MODULE' not in os.environ:
 
 django.setup()
 
-# from main.models import Platform
-#
-# with open("pyscripts/dg_metro.json") as data_file:
-#     data = json.load(data_file)
-#
-# # pprint(data['3']['칠곡경대병원'])
-# for line, p_dict in data.items():
-#     for platform, t_table in p_dict.items():
-#         Platform.objects.create(name=platform, line=line, time_table=str(t_table))
-# platform_list = Platform.objects.all()
-# platform_list.delete()
+from main.models import Platform
+
+with open("pyscripts/dg_metro.json") as data_file:
+    data = json.load(data_file)
+
+# pprint(data['3']['칠곡경대병원'])
+for line, p_dict in data.items():
+    for platform, t_table in p_dict.items():
+        Platform.objects.create(name=platform, line=line, time_table=str(t_table))
+platform_list = Platform.objects.all()
+platform_list.delete()
